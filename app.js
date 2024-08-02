a375259a033daf2fe696d9ea589b1f8d1ebbc7ea
Adam_Kalassina <kalassinaadam334@gmail.com>
2024-08-02 21:09:18 +0300

    End Edit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const xlsx = require("xlsx");
-
+const cors = require("cors");
 const wb = xlsx.readFile("employees.xlsx");
 const sheetName = wb.SheetNames[0]; // Assuming 'Sheet1' is the first sheet
 const ws = wb.Sheets[sheetName];
 const data = xlsx.utils.sheet_to_json(ws);
 // console.log(data);
-
+app.use(cors());
 app.use("/epmloyees_info", (req, res) => {
   res.send(data);
 });
