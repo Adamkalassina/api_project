1bd897157b9b515b5c0c5b51c2cd1525ce733e02
Adam_Kalassina <kalassinaadam334@gmail.com>
2024-08-02 20:42:05 +0300

    add cors to app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const xlsx = require("xlsx");
 
-const wb = xlsx.readFile(
-  "C:\\Users\\Lenovo\\Desktop\\Training Web0\\2024731\\employees.xlsx"
-);
+const wb = xlsx.readFile("employees.xlsx");
 const sheetName = wb.SheetNames[0]; // Assuming 'Sheet1' is the first sheet
 const ws = wb.Sheets[sheetName];
 const data = xlsx.utils.sheet_to_json(ws);
