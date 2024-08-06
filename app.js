a16890ac95e42167d934241041e2b5c6be94d92e
Adam_Kalassina <kalassinaadam334@gmail.com>
2024-08-06 18:21:17 +0300

    Change port

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const wb = xlsx.readFile("employees.xlsx");
 const sheetName = wb.SheetNames[0]; // Assuming 'Sheet1' is the first sheet
 const ws = wb.Sheets[sheetName];
 const data = xlsx.utils.sheet_to_json(ws);
-// console.log(data);
+
+const PORT = process.env.PORT || 3030;
+
+// your code
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
 app.use(cors());
 app.use("/epmloyees_info", (req, res) => {
   // res.send(data);
   res.send("Hello");
 });
-app.listen(3000);
